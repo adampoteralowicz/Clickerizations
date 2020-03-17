6d0743167b9135594e168783d4746d2cf39a6edd
adampoteralowicz <adam.poteralowicz2@gmail.com>
2020-03-17 01:00:32 +0100

    Preconfigured for Vue.js

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <h4 class="bg-primary text-black text-center p-2">Clickerizations</h4>
+        <div id = "map"></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'app'
+}
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiYnJlczE5OTAiLCJhIjoiY2s3OTJwZHduMGw3ZDNsbnl5d3gxZ3FiMiJ9.2Q9x3rLq6CR2bqYRYMwlWg';
+            var map = L.mapbox.map('map')
+                .setView([51.06, 17.02], 6)
+                .addLayer(L.mapbox.styleLayer('mapbox://styles/bres1990/ck79347ze06g11ir0och9fmi6'));
+</script>
